9bf6936b09989a230f1f582003c5c9f72bed6a60
elliotwoolley <elliot@attributesoftware.com>
2025-05-02 23:52:46 -0500

    remember users' theme preferences

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import "./styles/main.scss";
 import GameBoard from "./components/GameBoard/GameBoard.tsx";
 import ThemeToggleButton from "./components/ThemeToggleButton/ThemeToggleButton.tsx";
 
 export default function App() {
 
-    const [darkMode, setDarkMode] = useState(() => {
-        return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem('theme') ?? 'dark-mode';
     });
 
-    useEffect(() => {
-        if (darkMode) {
-            document.body.classList.add("dark-mode");
-        } else {
-            document.body.classList.remove("dark-mode");
-        }
-    }, [darkMode]);
+    function updateTheme(theme: string) {
+        localStorage.setItem('theme', theme);
+        setTheme(theme);
+    }
 
     return (
-        <div className={`app ${darkMode ? "dark-mode" : "light-mode"}`}>
-            <ThemeToggleButton darkMode={darkMode} setDarkMode={setDarkMode}/>
+        <div className={`app ${theme}`}>
+            <ThemeToggleButton theme={theme} updateTheme={updateTheme}/>
             <h1 className="title">QUORIDOR</h1>
             <GameBoard/>
         </div>
--- src/components/ThemeToggleButton/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton/ThemeToggleButton.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import MoonIcon from "../../assets/MoonIcon.tsx";
 import SunIcon from "../../assets/SunIcon.tsx";
 import styles from "./ThemeToggleButton.module.scss";
 
 interface ThemeToggleButtonProps {
-    setDarkMode: (darkMode: boolean) => void,
-    darkMode: boolean
+    theme: string,
+    updateTheme: (theme: string) => void
 }
 
-const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = ({ setDarkMode, darkMode }) => (
-    <button className={styles.themeToggleButton} onClick={() => setDarkMode(!darkMode)}
-            aria-label={darkMode ? "Switch to light mode" : "Switch to dark mode"}>
-        {darkMode ? <MoonIcon color={"var(--font-color)"}/> : <SunIcon color={"var(--font-color)"}/>}
-    </button>
-);
+const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = ({ theme, updateTheme }) => {
+
+    let ariaLabel: string;
+    let icon: ReactElement;
+    let onClick: () => void;
+
+    if (theme === "light-mode") {
+        ariaLabel = "Switch to dark mode";
+        icon = <SunIcon color={"var(--font-color)"}/>;
+        onClick = () => { updateTheme("dark-mode"); };
+    } else {
+        ariaLabel = "Switch to light mode";
+        icon = <MoonIcon color={"var(--font-color)"}/>;
+        onClick = () => { updateTheme("light-mode"); };
+    }
+
+    return (
+        <button className={styles.themeToggleButton} onClick={onClick}
+                aria-label={ariaLabel}>
+            {icon}
+        </button>
+    );
+};
 
 export default ThemeToggleButton;
